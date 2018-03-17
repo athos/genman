96c2468015b100e0a5883b1e11ce5bb68dd74356
OHTA Shogo <athos0220@gmail.com>
2018-03-17 14:44:01 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to genman
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/genman/core_test.clj
@@ -0,0 +1,7 @@
+(ns genman.core-test
+  (:require [clojure.test :refer :all]
+            [genman.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
