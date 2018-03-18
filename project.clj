8388d60f159037eb9926e0a268d5e3d0768351c6
OHTA Shogo <athos0220@gmail.com>
2018-03-18 19:42:40 +0900

    Update lib description & repo URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject genman "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Generator management utility for clojure.spec"
+  :url "https://github.com/athos/genman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
