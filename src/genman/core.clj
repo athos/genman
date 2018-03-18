41729ea1458c80850246d7bd3c0742f00e007197
OHTA Shogo <athos0220@gmail.com>
2018-03-18 12:23:22 +0900

    Implement basic generator management mechanism

--- src/genman/core.clj
+++ src/genman/core.clj
@@ -1,6 +1,32 @@
-(ns genman.core)
+(ns genman.core
+  (:require [clojure.spec.alpha :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def %gen-groups (atom {}))
+(def ^:dynamic *gen-group* :default)
+
+(def GENERATORS_SYM (gensym 'generators))
+
+(defmacro defgenerator
+  ([spec-name-or-path generator]
+   `(defgenerator *gen-group* ~spec-name-or-path ~generator))
+  ([gen-group spec-name-or-path generator]
+   `(do (swap! %gen-groups assoc-in [~gen-group '~spec-name-or-path]
+               (fn [] ~generator))
+        '~spec-name-or-path)))
+
+(defmacro with-gen-group [gen-group & body]
+  `(binding [*gen-group* ~gen-group]
+     ~@body))
+
+(defmacro use-gen-group [gen-group & body]
+  `(let [~GENERATORS_SYM (get @%gen-groups ~gen-group {})]
+     ~@body))
+
+(defmacro gen
+  ([spec] `(gen ~spec {}))
+  ([spec overrides]
+   `(let [overrides# (merge ~(if (get &env GENERATORS_SYM)
+                               GENERATORS_SYM
+                               `(get @%gen-groups *gen-group* {}))
+                            ~overrides)]
+      (s/gen ~spec overrides#))))
