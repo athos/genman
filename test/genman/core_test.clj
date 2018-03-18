8477443b6667e5e5c4b16d7b96d3ddd1b4636fdf
OHTA Shogo <athos0220@gmail.com>
2018-03-18 12:32:55 +0900

    Add unit tests for demonstrate generator management

--- test/genman/core_test.clj
+++ test/genman/core_test.clj
@@ -1,7 +1,48 @@
 (ns genman.core-test
-  (:require [clojure.test :refer :all]
-            [genman.core :refer :all]))
+  (:require [clojure.test :refer [is]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [com.gfredericks.test.chuck.clojure-test :refer [for-all]]
+            [clojure.spec.alpha :as s]
+            [genman.core :as genman :refer [defgenerator]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(s/def ::id int?)
+(s/def ::name string?)
+
+(defgenerator ::id
+  (s/gen (s/int-in 0 10000)))
+
+(genman/with-gen-group :test1
+  (defgenerator ::id
+    (gen/return 42))
+
+  (defgenerator ::name
+    (gen/return "foo")))
+
+(genman/with-gen-group :test2
+  (defgenerator ::id
+    (s/gen #{0 1 2}))
+
+  (defgenerator ::name
+    (gen/fmap #(apply str %)
+              (s/gen (s/coll-of char? :count 5)))))
+
+(defspec prop-default-gen-group
+  (for-all [id (genman/gen ::id)
+            name (genman/gen ::name)]
+    (is (<= 0 id 9999))
+    (is (string? name))))
+
+(defspec prop-test1-gen-group
+  (genman/use-gen-group :test1
+    (for-all [id (genman/gen ::id)
+              name (genman/gen ::name)]
+      (is (= id 42))
+      (is (= name "foo")))))
+
+(defspec prop-test2-gen-group
+  (genman/use-gen-group :test2
+    (for-all [id (genman/gen ::id)
+              name (genman/gen ::name)]
+      (is (contains? #{0 1 2} id))
+      (is (= (count name) 5)))))
