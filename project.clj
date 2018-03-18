1181a8d5c3837312f8a5957e05a14140a55fcae1
OHTA Shogo <athos0220@gmail.com>
2018-03-18 12:32:30 +0900

    Add deps

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]]
+  :profiles {:dev {:dependencies [[com.gfredericks/test.chuck "0.2.8"]]}})
